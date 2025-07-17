1711943ee9e0c3a49b2f27967e9827fb5f0da0cd
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-17 19:01:15 -0300

    Update ImageController.java
    Refactor:  Mudei a anotação de RequestMapping para GetMapping

--- src/main/java/com/willians/ListifyShop/controller/ImageController.java
+++ src/main/java/com/willians/ListifyShop/controller/ImageController.java
@@ -3,16 +3,13 @@ package com.willians.ListifyShop.controller;
 import com.willians.ListifyShop.utils.ImageOperator;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
 
-    @RequestMapping(value = "/{image}")
+    @GetMapping(value = "/{image}")
     public ResponseEntity<UrlResource> getImage(@PathVariable String image){
         ImageOperator imageOperator = new ImageOperator();
         return imageOperator.getImg(image);
