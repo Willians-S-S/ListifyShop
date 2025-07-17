55df41d1c02a3333caedfd0d81ed5436332da6e5
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-17 17:21:56 -0300

    Update UserResponseDto.java
    feat: adiciona campo urlImage ao UserResponseDto

--- src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
+++ src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
@@ -2,5 +2,5 @@ package com.willians.ListifyShop.dto;
 
 import java.util.UUID;
 
-public record UserResponseDto(UUID id, String name, String cpf, String email) {
+public record UserResponseDto(UUID id, String name, String cpf, String email, String urlImage) {
 }
