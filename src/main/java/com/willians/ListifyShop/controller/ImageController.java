4f31edc68051fcf2346bba2b474ea32efea8324e
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-17 23:14:34 -0300

    Update ImageController.java
    Feat: Rota para salvar uma imagem para um usuario

--- src/main/java/com/willians/ListifyShop/controller/ImageController.java
+++ src/main/java/com/willians/ListifyShop/controller/ImageController.java
@@ -1,13 +1,27 @@
 package com.willians.ListifyShop.controller;
 
+import com.willians.ListifyShop.dto.UserResponseDto;
+import com.willians.ListifyShop.service.ImageService;
 import com.willians.ListifyShop.utils.ImageOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/image")
 public class ImageController {
+
+    @Autowired
+    private ImageService imageService;
+
+    @PostMapping(value = "/{idUser}")
+    public ResponseEntity<UserResponseDto> saveImgUser(@PathVariable UUID idUser, @RequestPart("image") MultipartFile image){
+        return imageService.saveImgUser(idUser, image);
+    }
 
     @GetMapping(value = "/{image}")
     public ResponseEntity<UrlResource> getImage(@PathVariable String image){
