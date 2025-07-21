35646e377f6e7bc699698a385f7bccb47325bc67
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-21 19:07:24 -0300

    Create JwtService.java
    Feat: Cria JwtService: gera token JWT com escopos e identificador do usuário
    
        Implementa JwtService com método generateToken usando JwtEncoder.
    
        Extrai escopos das authorities e inclui o ID do usuário no token, quando disponível.
    
        Define tempo de expiração, emissor e campos customizados no payload.
    
        Suporte à autenticação segura baseada em JWT no sistema.

---
+++ src/main/java/com/willians/ListifyShop/security/authentication/JwtService.java
@@ -0,0 +1,63 @@
+package com.willians.ListifyShop.security.authentication;
+
+import com.willians.ListifyShop.entety.User;
+import com.willians.ListifyShop.security.userdetails.UserAuthenticated;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class JwtService {
+    private final JwtEncoder encoder;
+
+    public JwtService(JwtEncoder encoder){
+        this.encoder = encoder;
+    }
+
+    public String genereteToken(Authentication authentication){
+        Instant now = Instant.now();
+        long expire = 3600L;
+
+        String scopes = authentication.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
+        UUID userId = null;
+        if (authentication.getPrincipal() instanceof UserAuthenticated) {
+            UserAuthenticated user = (UserAuthenticated) authentication.getPrincipal();
+            userId = user.getId();
+        }
+
+//        var claims = JwtClaimsSet.builder()
+//                .issuer("spring-security-jwt")
+//                .issuedAt(now)
+//                .expiresAt(now.plusSeconds(expire))
+//                .subject(authentication.getName())
+//                .claim("scope", scopes)
+//                .build();
+
+        JwtClaimsSet.Builder claimsBuilder = JwtClaimsSet.builder()
+                .issuer("spring-security-jwt")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expire))
+                .subject(authentication.getName())
+                .claim("scope", scopes); // Adicione o escopo aqui também
+
+        // Adicione a claim "userId" ao builder ANTES de chamar .build()
+        if (userId != null) {
+            claimsBuilder.claim("id", userId);
+        }
+
+        var claims = claimsBuilder.build();
+
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+}
