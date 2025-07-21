4360f3226e7b4caede3ddb69da3bb1188ae09d06
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-21 19:12:42 -0300

    Update UserService.java
    Feat: Refatora UserService adiciona codificação de senha e atribuição de papel padrão
    
        Injeta PasswordEncoder e RoleService no UserService.
    
        Codifica a senha do usuário antes de persistir no banco de dados.
    
        Atribui automaticamente o papel USER ao novo usuário cadastrado.
    
        Altera método updateUser para utilizar id como parâmetro externo, garantindo mais segurança e clareza.

--- src/main/java/com/willians/ListifyShop/service/UserService.java
+++ src/main/java/com/willians/ListifyShop/service/UserService.java
@@ -3,15 +3,19 @@ package com.willians.ListifyShop.service;
 import com.willians.ListifyShop.dto.UserRequestDto;
 import com.willians.ListifyShop.dto.UserResponseDto;
 import com.willians.ListifyShop.dto.UserUpdate;
+import com.willians.ListifyShop.entety.Role;
 import com.willians.ListifyShop.entety.User;
+import com.willians.ListifyShop.enums.RoleName;
 import com.willians.ListifyShop.exception.NotFoundException;
 import com.willians.ListifyShop.mapstruct.UserMapper;
 import com.willians.ListifyShop.mapstruct.UserUpdateMapper;
 import com.willians.ListifyShop.repository.UserRepository;
+import com.willians.ListifyShop.security.config.SecurityConfig;
 import com.willians.ListifyShop.utils.ImageOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
@@ -30,6 +34,12 @@ public class UserService {
     @Autowired
     private UserUpdateMapper userUpdateMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleService roleService;
+
     public UserResponseDto addUser(UserRequestDto userRequest, MultipartFile image){
         this.userRepository.findByEmail(userRequest.email()).ifPresent(e -> {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Email já utilizado.");
@@ -40,6 +50,12 @@ public class UserService {
         });
 
         User user = mapper.requestToUser(userRequest);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role role = roleService.findByName(RoleName.USER);
+
+        user.getRoles().add(role);
 
         if(image != null){
             ImageOperator imageOperator = new ImageOperator();
@@ -68,8 +84,8 @@ public class UserService {
         return ResponseEntity.ok(usersResponse);
     }
 
-    public ResponseEntity<UserResponseDto> updateUser(UserUpdate userUpdate){
-        User user = this.userRepository.findById(userUpdate.id()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado."));
+    public ResponseEntity<UserResponseDto> updateUser(UUID id, UserUpdate userUpdate){
+        User user = this.userRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado."));
 
         userUpdateMapper.userUpdateToUser(userUpdate, user);
         this.userRepository.save(user);
