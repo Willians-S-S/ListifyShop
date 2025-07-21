0d6b247409eea8dfbe2b548d4e3739bc49d48461
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-21 19:00:36 -0300

    Update UserResponseDto.java
    Refactoe(UserResponseDto): adiciona lista de papéis (roles) ao retorno da API

--- src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
+++ src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
@@ -1,6 +1,14 @@
 package com.willians.ListifyShop.dto;
 
+import com.willians.ListifyShop.entety.Role;
+
+import java.util.List;
 import java.util.UUID;
 
-public record UserResponseDto(UUID id, String name, String cpf, String email, String urlImage) {
+public record UserResponseDto(UUID id,
+                              String name,
+                              String cpf,
+                              String email,
+                              String urlImage,
+                              List<Role> roles) {
 }
