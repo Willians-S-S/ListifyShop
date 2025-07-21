9028e08c71ab01cfa72a204cee86974e0b1c2d16
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-21 19:05:27 -0300

    Create AuthenticationService.java
    feat: Cria AuthenticationService gera token JWT a partir de autenticação
    
        Implementa AuthenticationService com método authenticate que delega geração de token ao JwtService.
    
        Injeta UserRepository e JwtService para futura expansão das lógicas de autenticação.
    
        Prepara camada de serviço para autenticação de usuários via JWT.

---
+++ src/main/java/com/willians/ListifyShop/security/authentication/AuthenticationService.java
@@ -0,0 +1,23 @@
+package com.willians.ListifyShop.security.authentication;
+
+import com.willians.ListifyShop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    JwtService jwtService;
+
+    public AuthenticationService(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+
+    public String autheticate(Authentication authentication){
+        return jwtService.genereteToken(authentication);
+    }
+}
