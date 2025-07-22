15ca00d67b350363c744e63cef522d042caef0e0
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-22 10:31:30 -0300

    Update UserUpdate.java
    Refactor: Foi alterado o tipo do id de UUID para String, mesmo com essa mudança o id é UUID, mas com o tipo String.

--- src/main/java/com/willians/ListifyShop/dto/UserUpdate.java
+++ src/main/java/com/willians/ListifyShop/dto/UserUpdate.java
@@ -1,6 +1,4 @@
 package com.willians.ListifyShop.dto;
 
-import java.util.UUID;
-
-public record UserUpdate(UUID id, String name, String cpf, String email, String password) {
+public record UserUpdate(String id, String name, String cpf, String email, String password) {
 }
