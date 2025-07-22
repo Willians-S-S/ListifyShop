145a61c5540e962949c9893a550a83c4d43e77fe
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-22 16:19:50 -0300

    Create UserDetailsImpl.java
    Style: Mudança no nome do arquivo

---
+++ src/main/java/com/willians/ListifyShop/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,67 @@
+package com.willians.ListifyShop.security.userdetails;
+
+import com.willians.ListifyShop.entety.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        /*
+         Este método converte a lista de papéis (roles) associados ao usuário
+         em uma coleção de GrantedAuthorities, que é a forma que o Spring Security
+         usa para representar papéis. Isso é feito mapeando cada papel para um
+         novo SimpleGrantedAuthority, que é uma implementação simples de
+         GrantedAuthority
+        */
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+    }
+
+    public String getId(){
+        return user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
