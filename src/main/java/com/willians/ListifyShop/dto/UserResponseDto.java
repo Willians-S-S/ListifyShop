a5e322d2d42ed3d09c9585027d35b8909384629b
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-22 10:31:24 -0300

    Update UserResponseDto.java
    Refactor: Foi alterado o tipo do id de UUID para String, mesmo com essa mudança o id é UUID, mas com o tipo String.

--- src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
+++ src/main/java/com/willians/ListifyShop/dto/UserResponseDto.java
@@ -3,9 +3,8 @@ package com.willians.ListifyShop.dto;
 import com.willians.ListifyShop.entety.Role;
 
 import java.util.List;
-import java.util.UUID;
 
-public record UserResponseDto(UUID id,
+public record UserResponseDto(String id,
                               String name,
                               String cpf,
                               String email,
