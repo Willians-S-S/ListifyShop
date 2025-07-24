d7c744902ca36853aa1c6359e2df10d971035b63
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-24 00:00:27 -0300

    Update data.sql
    Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,27 +6,27 @@ INSERT INTO role_table (id, name) VALUES
 
 -- USERS ----------------------------------------------------------
 -- senha = "123456" (BCrypt)
-INSERT INTO user_table (id, name, cpf, email, password, url_image) VALUES
+INSERT INTO user_table (id, name, cpf, email, password, url_image, creat_at, update_at) VALUES
   ('b1eebc99-9c0b-4ef8-bb6d-6bb9bd380b01',
    'Alice Santos',
    '123.456.789-09',
    'alice@listify.com',
    '$2a$12$v97p5npgCaHlHM0PZMLdCeKntEtUhiz7Qkt.EeG5a6c7XiSWkkXgy',
-   'https://cdn.listify.com/avatars/alice.png'),
+   'https://cdn.listify.com/avatars/alice.png', NOW(), NOW()),
 
   ('b2eebc99-9c0b-4ef8-bb6d-6bb9bd380b02',
    'Bruno Lima',
    '987.654.321-00',
    'bruno@listify.com',
    '$2a$10$V0./6xXkO7CjKQ8T7lHfbO6GGCtX7j6l7z5K8gYw4T9K3L7q7l9Lq',
-   NULL),
+   NULL, NOW(), NOW()),
 
   ('b3eebc99-9c0b-4ef8-bb6d-6bb9bd380b03',
    'Carla Oliveira',
    '111.222.333-44',
    'carla@listify.com',
    '$2a$10$V0./6xXkO7CjKQ8T7lHfbO6GGCtX7j6l7z5K8gYw4T9K3L7q7l9Lq',
-   'https://cdn.listify.com/avatars/carla.png');
+   'https://cdn.listify.com/avatars/carla.png', NOW(), NOW());
 
 -- RELAÇÕES user ↔ role ------------------------------------------
 -- Alice é ADMIN e USER
