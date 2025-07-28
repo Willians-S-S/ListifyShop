6b34722b703afe2e3e1aaa7818218ff05659aebf
Willians Silva <107960747+Willians-S-S@users.noreply.github.com>
2025-07-28 15:29:51 -0300

    Update PermissionLevel.java
    Fix

--- src/main/java/com/willians/ListifyShop/enums/PermissionLevel.java
+++ src/main/java/com/willians/ListifyShop/enums/PermissionLevel.java
@@ -1,5 +1,23 @@
 package com.willians.ListifyShop.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PermissionLevel {
-    READ, WRITE
+    READ, WRITE;
+
+    @JsonValue
+    public String getValue() {
+        return this.name();  // Retorna o nome do enum como string
+    }
+
+    @JsonCreator
+    public static PermissionLevel forValue(String value) {
+        for (PermissionLevel level : PermissionLevel.values()) {
+            if (level.name().equalsIgnoreCase(value)) {
+                return level;
+            }
+        }
+        throw new IllegalArgumentException("Unknown value: " + value);
+    }
 }
